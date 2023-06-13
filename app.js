0e470f8bcf8b90b9945ba98ebc37c03bf6789ebe
jaydip mokariya <mokariyajaydip9@gmail.com>
2023-06-13 19:31:15 +0530

    add mongoDB as Database

--- app.js
+++ app.js
@@ -3,9 +3,21 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
-var lodash= require('lodash');
+const lodash= require('lodash');
+const mongoose=require("mongoose");
 
-let posts=[];
+
+mongoose.connect("mongodb://127.0.0.1:27017/test");
+
+const blogScheme=mongoose.Schema({
+  title:String,
+  content:String
+})
+
+
+let blog=mongoose.model('blog',blogScheme);
+
+
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
@@ -18,7 +30,10 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 app.get('/',(req,res)=>{
-  res.render('home.ejs',{homeInfo:homeStartingContent,posts:posts});
+  blog.find({}).then((items)=>{
+    res.render('home.ejs',{homeInfo:homeStartingContent,posts:items});
+  })
+  
 })
 
 app.get('/contact',(req,res)=>
@@ -36,31 +51,40 @@ app.get('/compose',(req,res)=>{
 })
 app.post('/compose',(req,res)=>
 {
+
+   let pt=new blog({
+    title:req.body.postTitle,
+    content:req.body.postContent
+   })
+   pt.save();
   console.log(req.body.postTitle);
-  let post={"title":req.body.postTitle,"content":req.body.postContent};
-  posts.push(post);
   res.render('compose.ejs');
 })
 
 app.get('/posts/:id',(req,res)=>{
   
-  let flag=true;
-  for(let i=0;i<posts.length;i++)
+  blog.find({}).then((items)=>
   {
-    if(lodash.lowerCase(posts[i]["title"])==lodash.lowerCase(req.params.id))
+    let posts=items;
+      let flag=true;
+    for(let i=0;i<posts.length;i++)
     {
-      res.render('post.ejs',{post:posts[i]});
-      flag=false;
+      if(lodash.lowerCase(posts[i]["title"])==lodash.lowerCase(req.params.id))
+      {
+        res.render('post.ejs',{post:posts[i]});
+        flag=false;
+      }
     }
-  }
- 
-    if(flag)
-    {
-    res.write("Invalid Post Request")
-    res.send();
+  
+      if(flag)
+      {
+      res.write("Invalid Post Request")
+      res.send();
 
-    }
-    
+      }
+
+  })
+
 })
 
 
